e68bb8e3088d25e12995f3ba824e08b4464d90dd
rahul1290 <sinharahulsinha.sinha@gmail.com>
2020-12-31 06:35:22 +0530

    setup web-server

---
+++ web-server/src/app.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicDir = path.join(__dirname,'../public');  
+
+app.use(express.static(publicDir));
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello world</h1>');
+})
+
+app.listen(3002,()=>{
+    console.log('web server running on port 3002');
+})
