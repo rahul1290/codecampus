5929ba4b1b8b30d8d91b59e89659497a69789769
rahul121290 <sinha.rahulsinha.sinha@gmail.com>
2021-01-08 11:40:53 +0530

    OMDB page created

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,9 +6,9 @@ const publicDir = path.join(__dirname,'../public');
 
 app.use(express.static(publicDir));
 
-app.get('/',(req,res)=>{
+/*app.get('/',(req,res)=>{
     res.send('<h1>Hello world</h1>');
-})
+})*/
 
 app.listen(3002,()=>{
     console.log('web server running on port 3002');
